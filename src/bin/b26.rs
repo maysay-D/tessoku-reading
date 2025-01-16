d8d5a3d2185ca4776e48cace7bfc77948af33b1b
maysay-D <22j5085@stu.meisei-u.ac.jp>
2025-01-16 18:42:38 +0900

    feat: Solve B26

--- src/bin/b26.rs
+++ src/bin/b26.rs
@@ -1,4 +1,5 @@
 #![allow(unused_imports)]
+#![allow(unused_variables)]
 #![allow(non_snake_case)]
 use ac_library::*;
 use once_cell::sync::Lazy;
@@ -65,17 +66,34 @@ use rustc_hash::*;
 use smallvec::*;
 
 fn main() {
-    // 問題に応じて変更する
     input! {
-        n: usize,
-        a: [isize; n],
+        N: usize,
     }
 
-    let ans = solve(n, &a);
-    println!("{}", ans);
+    let ans = solve(N);
+    for a in ans {
+        println!("{}", a);
+    }
 }
 
-// 問題に応じて変更する
-fn solve(n: usize, a: &[isize]) -> isize {
-    todo!()
+fn solve(N: usize) -> Vec<usize> {
+    let result = eratosthenes_sieve(N);
+    result
+}
+
+fn eratosthenes_sieve(n: usize) -> Vec<usize> {
+    let mut prime_tab = vec![true; n + 1];
+    prime_tab.iter_mut().take(2).for_each(|x| *x = false);
+    for i in 2..=n.sqrt() {
+        if prime_tab[i] {
+            for j in (i * i..=n).step_by(i) {
+                prime_tab[j] = false;
+            }
+        }
+    }
+    prime_tab
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &x)| if x { Some(i) } else { None })
+        .collect()
 }
